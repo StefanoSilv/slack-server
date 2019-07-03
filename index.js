d8b00192efa89c1b980d5b2e27dafc4ef0f1316e
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-03 07:13:43 +0200

    api added for messages

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express()
 require('dotenv').config()
 
 
+app.get('/api/messages', (req,res)=> { res.send('Hello') })
+
+
+
+
+
+
+
+
 app.listen(process.env.PORT, () => {
 	console.log(`Server listening on port ${process.env.PORT}`);
 })
