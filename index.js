c3362210db05d25b9dbb769e892bf68d3372fe48
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-03 06:38:07 +0200

    git commit -m”server running”

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+require('dotenv').config()
+
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on port ${process.env.PORT}`);
+})
